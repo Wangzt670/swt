4ad77fdc270e383b566a071bbfff271a3291c0dc
wzt <670170128@qq.com>
2022-06-18 11:11:56 +0800

    Initial commit

---
+++ src/main/java/com/cqu/swt/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.cqu.swt.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+    /**
+     * 设置静态资源映射
+     * @param registry
+     */
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+}

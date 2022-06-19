2e1a9bf032174cb56995d05a8e657548359bde18
wzt <670170128@qq.com>
2022-06-19 15:56:46 +0800

    完成登录界面所有功能开发

--- src/main/java/com/cqu/swt/controller/EmployeeController.java
+++ src/main/java/com/cqu/swt/controller/EmployeeController.java
@@ -61,4 +61,15 @@ public class EmployeeController {
         return R.success(emp);
     }
 
+    /**
+     * 员工退出
+     * @param request
+     * @return
+     */
+    @PostMapping("/logout")
+    public R<String> logout(HttpServletRequest request){
+        //清理Session中保存的当前登录员工的id
+        request.getSession().removeAttribute("employee");
+        return R.success("退出成功");
+    }
 }
---
+++ src/main/java/com/cqu/swt/filter/LoginCheckFilter.java
@@ -0,0 +1,81 @@
+package com.cqu.swt.filter;
+
+import com.alibaba.fastjson.JSON;
+import com.cqu.swt.common.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.AntPathMatcher;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 检查用户是否已经完成登录
+ */
+@WebFilter(filterName = "loginCheckFilter",urlPatterns = "/*")
+@Slf4j
+public class LoginCheckFilter implements Filter {
+
+    //路径匹配器，支持通配符，进行路径匹配
+    public static final AntPathMatcher PATH_MATCHER = new AntPathMatcher();
+
+    //继承过滤方法
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        //1、获取本次请求的URI
+        String requestURI = request.getRequestURI();
+        //log.info("拦截到请求：{}",requestURI);
+
+        //定义不需要处理的请求路径
+        String[] urls = new String[]{
+                "/employee/login",  //登录
+                "/employee/logout",//退出
+                "/backend/**",//静态资源
+                "/front/**"//静态资源
+        };
+
+        //2、判断本次请求是否需要处理
+        boolean check = check(urls, requestURI);
+
+        //3、如果不需要处理，则直接放行
+        if(check){
+            //log.info("本次请求{}不需要处理",requestURI);
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        //4、判断登录状态，如果已登录，则直接放行
+        if(request.getSession().getAttribute("employee") != null){
+            //log.info("用户已登录，用户id为：{}",request.getSession().getAttribute("employee"));
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        //log.info("用户未登录");
+        //5、如果未登录则返回未登录结果，通过输出流方式向客户端页面响应数据
+        response.getWriter().write(JSON.toJSONString(R.error("NOTLOGIN")));
+        return;
+
+    }
+
+    /**
+     * 路径匹配，检查本次请求是否需要放行
+     * @param urls
+     * @param requestURI
+     * @return
+     */
+    public boolean check(String[] urls,String requestURI){
+        for (String url : urls) {
+            boolean match = PATH_MATCHER.match(url, requestURI);
+            if(match){
+                return true;
+            }
+        }
+        return false;
+    }
+}

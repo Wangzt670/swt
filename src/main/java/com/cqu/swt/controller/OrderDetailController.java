458eec7b0ec6070a4b13dad5d8688de3306f8f49
オレンジ_Mint <hanyi010126@outlook.com>
2022-06-28 18:36:17 +0800

    用户下单 接口注释

--- src/main/java/com/cqu/swt/controller/OrderDetailController.java
+++ src/main/java/com/cqu/swt/controller/OrderDetailController.java
@@ -3,6 +3,9 @@ package com.cqu.swt.controller;
 import com.cqu.swt.common.R;
 import com.cqu.swt.entity.OrderDetail;
 import com.cqu.swt.service.OrderDetailService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +18,7 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequestMapping("/orderDetail")
+@Api(value = "订单内容相关接口")
 public class OrderDetailController {
 
     @Autowired
@@ -22,6 +26,8 @@ public class OrderDetailController {
 
 
     @GetMapping("/{id}")
+    @ApiOperation("获取主键")
+    @ApiImplicitParam(name = "id", value = "主键", required = true)
     public R<List<OrderDetail>> getById(@PathVariable Long id){
 
         return  orderDetailService.getByOrderId(id);

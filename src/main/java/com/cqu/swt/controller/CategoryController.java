0561a2bda769a6e316455ee70ccbd4dca26d3e30
cj <cqucj163@163.com>
2022-06-28 10:32:32 +0800

    接口注释添加

--- src/main/java/com/cqu/swt/controller/CategoryController.java
+++ src/main/java/com/cqu/swt/controller/CategoryController.java
@@ -5,6 +5,10 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.cqu.swt.common.R;
 import com.cqu.swt.entity.Category;
 import com.cqu.swt.service.CategoryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +21,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/category")
 @Slf4j
+@Api(tags = "分类相关接口")
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
@@ -27,6 +32,7 @@ public class CategoryController {
      * @return
      */
     @PostMapping
+    @ApiOperation(value = "新增分类接口")
     public R<String> save(@RequestBody Category category){
         log.info("category:{}",category);
         categoryService.save(category);
@@ -40,6 +46,11 @@ public class CategoryController {
      * @return
      */
     @GetMapping("/page")
+    @ApiOperation(value = "分类分页查询接口")
+    @ApiImplicitParams({
+                @ApiImplicitParam(name = "page",value = "页码",required = true),
+                @ApiImplicitParam(name = "pageSize",value = "每页记录数",required = true)
+            })
     public R<Page> page(int page,int pageSize){
         //分页构造器
         Page<Category> pageInfo = new Page<>(page,pageSize);
@@ -58,6 +69,7 @@ public class CategoryController {
      * @return
      */
     @DeleteMapping
+    @ApiOperation(value = "删除分类接口")
     public R<String> delete(Long ids) {
         log.info("删除分类，id为{}",ids);
 //        categoryService.removeById(ids);
@@ -71,6 +83,7 @@ public class CategoryController {
      * @return
      */
     @PutMapping
+    @ApiOperation(value = "修改分类接口")
     public R<String> update(@RequestBody Category category)
     {
         log.info("修改分类信息：{}",category);
@@ -84,6 +97,7 @@ public class CategoryController {
      * @return
      */
     @GetMapping("/list")
+    @ApiOperation(value = "查询分类接口")
     public R<List<Category>> list(Category category){
         //条件构造器
         LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();

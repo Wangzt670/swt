9726f3b5ed37739324ed338988dcbb790d2ced0d
cj <cqucj163@163.com>
2022-06-29 14:33:51 +0800

    C阶段源代码添加

---
+++ 项目待交付工件/C构建阶段/1.各成员源代码/崔晋/CategoryController.java
@@ -0,0 +1,112 @@
+package com.cqu.swt.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.cqu.swt.common.R;
+import com.cqu.swt.entity.Category;
+import com.cqu.swt.service.CategoryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * 分类管理
+ */
+@RestController
+@RequestMapping("/category")
+@Slf4j
+@Api(tags = "分类相关接口")
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    /**
+     * 新增分类（返回json类型数据）
+     * @param category
+     * @return
+     */
+    @PostMapping
+    @ApiOperation(value = "新增分类接口")
+    public R<String> save(@RequestBody Category category){
+        log.info("category:{}",category);
+        categoryService.save(category);
+        return R.success("新增分类成功");
+    }
+
+    /**
+     * 分页查询
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    @GetMapping("/page")
+    @ApiOperation(value = "分类分页查询接口")
+    @ApiImplicitParams({
+                @ApiImplicitParam(name = "page",value = "页码",required = true),
+                @ApiImplicitParam(name = "pageSize",value = "每页记录数",required = true)
+            })
+    public R<Page> page(int page,int pageSize){
+        //分页构造器
+        Page<Category> pageInfo = new Page<>(page,pageSize);
+        //条件构造器
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+        //添加排序条件，根据sort进行排序
+        queryWrapper.orderByAsc(Category::getSort);
+        //分页查询
+        categoryService.page(pageInfo,queryWrapper);
+        return R.success(pageInfo);
+    }
+
+    /**
+     * 根据id删除分类
+     * @param ids
+     * @return
+     */
+    @DeleteMapping
+    @ApiOperation(value = "删除分类接口")
+    public R<String> delete(Long ids) {
+        log.info("删除分类，id为{}",ids);
+//        categoryService.removeById(ids);
+        categoryService.remove(ids);
+        return R.success("分类信息删除成功");
+    }
+
+    /**
+     * 根据id修改分类信息
+     * @param category
+     * @return
+     */
+    @PutMapping
+    @ApiOperation(value = "修改分类接口")
+    public R<String> update(@RequestBody Category category)
+    {
+        log.info("修改分类信息：{}",category);
+        categoryService.updateById(category);
+        return R.success("修改分类信息成功");
+    }
+
+    /**
+     * 根据条件查询分类数据
+     * @param category
+     * @return
+     */
+    @GetMapping("/list")
+    @ApiOperation(value = "查询分类接口")
+    public R<List<Category>> list(Category category){
+        //条件构造器
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+        //添加条件
+        queryWrapper.eq(category.getType() != null,Category::getType,category.getType());
+        //添加排序条件
+        queryWrapper.orderByAsc(Category::getSort).orderByDesc(Category::getUpdateTime);
+
+        List<Category> list = categoryService.list(queryWrapper);
+        return R.success(list);
+    }
+}
---
+++ 项目待交付工件/C构建阶段/1.各成员源代码/崔晋/CategoryMapper.java
@@ -0,0 +1,10 @@
+package com.cqu.swt.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.cqu.swt.entity.Category;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CategoryMapper extends BaseMapper<Category> {
+
+}

f900c1f2bacaeec4ecf0639dafbfeb69991d6f7b
wkf-87 <77309729+wkf-87@users.noreply.github.com>
2022-06-29 17:12:33 +0800

    修改显示

--- src/main/java/com/cqu/swt/filter/LoginCheckFilter.java
+++ src/main/java/com/cqu/swt/filter/LoginCheckFilter.java
@@ -60,7 +60,7 @@ public class LoginCheckFilter implements Filter {
 
         //4-1、判断登录状态，如果已登录，则直接放行
         if(request.getSession().getAttribute("employee") != null){
-            log.info("用户已登录，用户id为：{}",request.getSession().getAttribute("employee"));
+            log.info("员工已登录，用户id为：{}",request.getSession().getAttribute("employee"));
             Long empId = (Long)request.getSession().getAttribute("employee");
             BaseContext.setCurrentId(empId);
 

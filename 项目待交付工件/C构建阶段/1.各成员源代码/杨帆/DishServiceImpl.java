ce4023c4e09c7f2ba5c03743320950324abaac64
handsomefan1 <2642413273@qq.com>
2022-07-01 10:22:09 +0800

    个人源码

---
+++ 项目待交付工件/C构建阶段/1.各成员源代码/杨帆/DishServiceImpl.java
@@ -0,0 +1,123 @@
+package com.cqu.swt.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.cqu.swt.common.CustomException;
+import com.cqu.swt.dto.DishDto;
+import com.cqu.swt.entity.Dish;
+import com.cqu.swt.entity.DishFlavor;
+import com.cqu.swt.mapper.DishMapper;
+import com.cqu.swt.service.DishFlavorService;
+import com.cqu.swt.service.DishService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+public class DishServiceImpl extends ServiceImpl<DishMapper,Dish> implements DishService {
+
+    @Autowired
+    private DishFlavorService dishFlavorService;
+
+    /**
+     * 新增菜品，同时保存对应的口味数据
+     * @param dishDto
+     */
+    @Transactional
+    public void saveWithFlavor(DishDto dishDto) {
+        //保存菜品的基本信息到菜品表dish
+        this.save(dishDto);
+
+        Long dishId = dishDto.getId();//菜品id
+
+        //菜品口味
+        List<DishFlavor> flavors = dishDto.getFlavors();
+        flavors = flavors.stream().map((item) -> {
+            item.setDishId(dishId);
+            return item;
+        }).collect(Collectors.toList());
+
+        //保存菜品口味数据到菜品口味表dish_flavor
+        dishFlavorService.saveBatch(flavors);
+
+    }
+
+    /**
+     * 根据id查询菜品信息和对应的口味信息
+     * @param id
+     * @return
+     */
+    public DishDto getByIdWithFlavor(Long id) {
+        //查询菜品基本信息，从dish表查询
+        Dish dish = this.getById(id);
+
+        DishDto dishDto = new DishDto();
+        BeanUtils.copyProperties(dish,dishDto);
+
+        //查询当前菜品对应的口味信息，从dish_flavor表查询
+        LambdaQueryWrapper<DishFlavor> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(DishFlavor::getDishId,dish.getId());
+        List<DishFlavor> flavors = dishFlavorService.list(queryWrapper);
+        dishDto.setFlavors(flavors);
+
+        return dishDto;
+    }
+
+    @Override
+    @Transactional
+    public void updateWithFlavor(DishDto dishDto) {
+        //更新dish表基本信息
+        this.updateById(dishDto);
+
+        //清理当前菜品对应口味数据---dish_flavor表的delete操作
+        LambdaQueryWrapper<DishFlavor> queryWrapper = new LambdaQueryWrapper();
+        queryWrapper.eq(DishFlavor::getDishId,dishDto.getId());
+
+        dishFlavorService.remove(queryWrapper);
+
+        //添加当前提交过来的口味数据---dish_flavor表的insert操作
+        List<DishFlavor> flavors = dishDto.getFlavors();
+
+        flavors = flavors.stream().map((item) -> {
+            item.setDishId(dishDto.getId());
+            return item;
+        }).collect(Collectors.toList());
+
+        dishFlavorService.saveBatch(flavors);
+    }
+
+
+    /**
+     * 删除菜品
+     * @param
+     */
+    @Transactional
+    public void removeWithFlavor(List<Long> ids) {
+        ///select count(*) from dish where id in (1,2,3) and status = 1
+        //查询菜品状态，确定是否可用删除
+        LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper();
+        queryWrapper.in(Dish::getId,ids);
+        queryWrapper.eq(Dish::getStatus,1);
+
+        int count = this.count(queryWrapper);
+        if(count > 0){
+            //如果不能删除，抛出一个业务异常
+            throw new CustomException("菜品正在售卖中，不能删除");
+        }
+
+        //如果可以删除，先删除套餐表中的数据---dish
+        this.removeByIds(ids);
+
+        //delete from dish_flavor where dish_id in (1,2,3)
+        LambdaQueryWrapper<DishFlavor> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.in(DishFlavor::getDishId,ids);
+        //删除关系表中的数据----dish_flavor
+        dishFlavorService.remove(lambdaQueryWrapper);
+    }
+}
